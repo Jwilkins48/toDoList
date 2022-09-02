9b826a0b43e236e6e8c841e00ad077c084e90225
Jenna <jenna48071@gmail.com>
2022-09-01 20:53:11 -0500

    changed color scheme

--- src/components/AllTasks.jsx
+++ src/components/AllTasks.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import AddTaskDropdown from './AddTaskDropdown';
 import Card from './Card';
-import { useState } from 'react';
 
 function AllTasks({open={open}, setOpen={setOpen}, defaultTasks, addTask, deleteTask, editTask}) {
 
 return (
 <div>
-  <h2 className='allTaskHeader'>{defaultTasks.length} Things To Do</h2>
+  <h2 className='allTaskHeader'><span className='numberSpan'>{defaultTasks.length}</span> Things To Do</h2>
 
   {defaultTasks.map(task => (<Card
   key={task.id} task={task} id={task.id} editTask={editTask} deleteTask={deleteTask} defaultTasks={defaultTasks}/>))}
